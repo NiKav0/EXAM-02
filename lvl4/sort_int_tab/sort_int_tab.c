2bdb80e459f9a0efbec292e2c6c495308b876999
NiKav0 <105223490+NiKav0@users.noreply.github.com>
2024-01-11 04:16:18 +0100

    Level 4

---
+++ lvl4/sort_int_tab/sort_int_tab.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+
+void	sort_int_tab(int *tab, unsigned int size)
+{
+	unsigned int i = 0;
+	int tmp;
+
+	while (i < (size -1))
+	{
+		if (tmp[i] > tmp[i + 1])
+		{
+			tmp = tab[i];
+			tab[i] = tab[i + 1];
+			tab[i + 1] = tmp;
+			i = 0;
+		}
+		else
+			i++;
+	}
+}
+
